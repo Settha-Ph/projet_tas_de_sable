619781474f6519ff9f2a2279cda88282ffdfe650
Astrid LF <astrid.la-fonta@ens.uvsq.fr>
2022-02-11 11:10:58 +0100

    j'ai mis un point

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -2,7 +2,7 @@
 # groupe MPCI 3
 # Settha PHOUPHETLINTHONG
 # Elisabth MIGNOT
-# Astrid LA FONTA
+# Astrid LA FONTA   . 
 # etc...
 # https://github.com/Settha-Ph/projet_tas_de_sable.git
 #########################################

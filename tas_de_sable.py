8449ec5995235c3e9ad7fa1117cf42db515c8fd9
OmbreAstrale <35828767+OmbreAstrale@users.noreply.github.com>
2022-02-11 11:16:18 +0100

    tentative de correction du merge conflict

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,13 +1,10 @@
 #########################################
 # groupe MI 4
 # Settha PHOUPHETLINTHONG
-<<<<<<< HEAD
 # Elisabth MIGNOT
 # Astrid LA FONTA   . 
-=======
-# Elisabth MIGNOT : bonjour
+# Elisabeth MIGNOT : bonjour
 # Astrid LA FONTA
->>>>>>> d89512ee8c198c6a54389dbeb9dd0c964f1c7071
 # etc...
 # https://github.com/Settha-Ph/projet_tas_de_sable.git
 #########################################

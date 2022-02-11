90610982616e6c5bc551a2a0eafc6bd0c247a5ac
ValoneNot <elili010299@gmail.com>
2022-02-11 11:07:38 +0100

    modif Elisabeth

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,7 +1,7 @@
 #########################################
 # groupe MPCI 3
 # Settha PHOUPHETLINTHONG
-# Elisabth MIGNOT
+# Elisabth MIGNOT : bonjour
 # Astrid LA FONTA
 # etc...
 # https://github.com/Settha-Ph/projet_tas_de_sable.git

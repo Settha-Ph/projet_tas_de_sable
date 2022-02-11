afcf4380c4e173deb9a90310fa39556663c02906
ValoneNot <elili010299@gmail.com>
2022-02-11 11:17:28 +0100

    suppretion double ligne

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -1,10 +1,8 @@
 #########################################
 # groupe MI 4
 # Settha PHOUPHETLINTHONG
-# Elisabth MIGNOT
-# Astrid LA FONTA   . 
 # Elisabeth MIGNOT : bonjour
-# Astrid LA FONTA
+# Astrid LA FONTA   . 
 # etc...
 # https://github.com/Settha-Ph/projet_tas_de_sable.git
 #########################################

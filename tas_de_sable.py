35caea60227abb84ca9bd4e02a764911e0775eb7
ValoneNot <elili010299@gmail.com>
2022-02-11 12:40:31 +0100

    debut programe par Elisabeth (tkinter, liste, remplissage liste)

--- tas_de_sable.py
+++ tas_de_sable.py
@@ -10,7 +10,8 @@
 ###########
 # IMPORT DES LIBRAIRIES
 
-
+import tkinter as tk
+import random as rd
 
 ###########
 # DEFINITION DES CONSTANTES
@@ -20,12 +21,43 @@
 ##########
 # DEFINITION DES VARIABLES GLOBALES
 
+liste_TDS = []
+for i in range(5):
+    liste_2=[]
+    a="c"
+    for j in range(5):
+        if (i == 0 or i == 4) and (j == 0 or j == 4):
+            a = ""
+        elif (i > 0 or i < 4) and (j == 0 or j == 4):
+            a = "#"
+        elif (i == 0 or i == 4) and (j > 0 or j < 4):
+            a = "#"
+        else :
+            a = rd.randint(0,9)
+        liste_2.append(a)
+    liste_TDS.append(liste_2)
+    print(liste_2)
+    liste_2=[]
 
 
 ##########
 # DEFINITION DES FONCTIONS
 
-
+def fct():
+    pass
 
 ##########
 # MAIN
+
+"""racine = tk.Tk()
+
+canvas = tk.Canvas(racine, width = 500, height = 500, bg = "white")
+canvas.grid(row = 1, column = 1)
+
+boutton = tk.Button(racine, command = fct, text="configuration courante")
+boutton.grid(row = 2, column = 1)
+
+
+#canvas.create_text(250,250, text="yo")
+
+racine.mainloop()"""
